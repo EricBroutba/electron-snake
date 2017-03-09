34d945f73631d6b17919356bf118ead4a5e9f40d
Eric <paralysant@gmail.com>
2017-03-09 15:56:22 +0100

    updated struct

--- app/controller.js
+++ app/controller.js
@@ -1,3 +1,5 @@
-export default class Snake {
-    
+
+
+function init(){
+    var snakeBody = document.getElementsByClassName("snakeBody");
 }
--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createWindow(){
     win = new BrowserWindow({width: 800, height: 600})
     // and load the index.html of the app.
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, 'app/index.html'),
         protocol: 'file:',
         slashes: true
     }))
