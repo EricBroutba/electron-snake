731757a8ad0d37ccb289870d26fb8b702db6e669
Eric <paralysant@gmail.com>
2017-03-10 15:18:34 +0100

    Snake class WIP

--- app/controller.js
+++ app/controller.js
@@ -1,6 +1,37 @@
 // var globalShortcut = require('global-shortcut');
 console.log("LOADED")
+class Snake{
+    constructor(container, posX, posY, mvDir){
+        this.container = container;
+        this.posX = posX;
+        this.posY = posY;
+        this.mvDir = mvDir;
+    }
+    mv(e){
+        console.log(e.code)
+        switch (e.code) {
+            case "ArrowUp":
+                console.log("UUUUUP");
+                break;
+            case "ArrowLeft":
+                console.log("LEFT");
+                break;
+
+            case "ArrowRight":
+                console.log("RIGHT");
+                break;
+
+            case "ArrowDown":
+                console.log("DOWN");
+                break;
+        }
+    }
+}
+
 function initSnake(){
-    var snakeBody = document.getElementsByClassName("snakeBody");
+
+    var snakeBody = document.getElementById("snakeBody");
     console.log(snakeBody);
+    var snake = new Snake(snakeBody, 50, 50, "right")
+    document.onkeydown = snake.mv;
 }
