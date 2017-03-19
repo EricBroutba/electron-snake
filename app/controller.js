6a97280bd1fe5d085e14e8f1d7b3c6b380f97216
ericBroutba <eric.bousbaa@hotmail.com>
2017-03-19 21:00:46 +0100

    food WIP

--- app/controller.js
+++ app/controller.js
@@ -1,26 +1,81 @@
 // var globalShortcut = require('global-shortcut');
-
 function Snake (container, posX, posY, mvDir, speed){
     mvDir = (typeof mvDir !== 'undefined') ?  mvDir : "right";
     speed = (typeof speed !== 'undefined') ?  speed : "500";
     this.container = container;
+    posX = 100;
+    posY = 100;
     this.container.style.left = posX;
     this.container.style.top = posY;
     this.direction = mvDir;
     this.speed = speed;
+    this.snakeLength = 1;
+    this.generateFood();
+
 }
-Snake.prototype.didWeHitSomething = function(){
+
+Snake.prototype.generateFood = function(){
+    if(document.contains(document.getElementById("apple"))){
+        console.log("REMOVING FOOD")
+        document.getElementById("apple").remove();
+    }
+
+    var foodElement = document.createElement("div");
+    foodElement.setAttribute("id", "apple");
+    foodElement.style.position = "absolute";
+
+
+    let foodPosX = Math.floor(Math.random(0, window.innerHeight)*10)*20;
+    let foodPosY = Math.floor(Math.random(0, window.innerWidth)*10)*20;
+    foodPosX = 100;
+    foodPosY = 100;
+    this.foodPosX = foodPosX;
+    this.foodPosY = foodPosY;
+    foodElement.style.left = foodPosX;
+    foodElement.style.top = foodPosY;
+
+    foodElement.style.width = "20px";
+    foodElement.style.height = "20px";
+    foodElement.style.backgroundColor = "red";
+
+    document.body.appendChild(foodElement);
+
+}
+Snake.prototype.didWeHitAWall = function(){
     let posX = parseInt(this.container.style.left);
     let posY = parseInt(this.container.style.top);
-    let didWe;
+    let weDid;
 
-    didWe = posX <= 0 ? true : (posX >= window.innerWidth ? true : false);
+    //TODO precisions with window.innerWidth and windown.innerHeight
 
-    didWe = posY <= 0 ? true : (posY >= window.innerHeight ? true : false);
-    //return posY >= window.innerHeight ? true : false;
-    //console.log(posX <= 0)
-    //console.log(posX >= window.innerWidth)
-    return didWe;
+    weDid = posX <= 0 ? true : (posX >= window.innerWidth ? true : false);
+    if(weDid){
+        return true;
+    }
+    weDid = posY <= 0 ? true : (posY >= window.innerHeight ? true : false);
+    if(weDid){
+        return true;
+    }
+
+    return false;
+};
+Snake.prototype.didWeAte = function(){
+
+    let posX = parseInt(this.container.style.left);
+    let posY = parseInt(this.container.style.top);
+
+    if((posX == this.foodPosX) && (posY == this.foodPosY)){
+        this.snakeLength += 1;
+        console.log("WE JUST ATE")
+
+        console.log("YYY")
+        console.log(posY);
+        console.log(this.foodPosY);
+        console.log("XXX")
+        console.log(posX)
+        console.log(this.foodPosY)
+
+    }
 };
 Snake.prototype.mvEvent = function(e){
     switch (e.code) {
@@ -52,9 +107,11 @@ Snake.prototype.mvEvent = function(e){
 }
 
 Snake.prototype.mvAction = function(){
-    // console.log("X : " + this.container.style.left);
-    // console.log("Y : " + this.container.style.top);
-    console.log(this.didWeHitSomething());
+
+    if (this.didWeHitAWall()){
+        console.log("GAME OVER");
+    }
+    this.didWeAte();
 
 
     switch (this.direction){
@@ -80,6 +137,7 @@ function initSnake(){
 
     var snakeBody = document.getElementById("snakeBody");
     var snake = new Snake(snakeBody, 100, 100, null, "300");
+
 
     document.onkeydown = snake.mvEvent.bind(snake);
     // credits to http://stackoverflow.com/questions/20279484/how-to-access-the-correct-this-inside-a-callback
